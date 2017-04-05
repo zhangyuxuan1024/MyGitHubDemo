eab62cd5d567bb4f38b3cb6905d2abeb4c6805bc
zhangyuxuan1024 <zhangyuxuan1024@163.com>
2017-04-05 17:36:49 +0800

    这次绑定了TextView

--- app/src/main/java/net/zyxdemo/mygithubdemo/MainActivity.java
+++ app/src/main/java/net/zyxdemo/mygithubdemo/MainActivity.java
@@ -1,13 +1,17 @@
 package net.zyxdemo.mygithubdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+        tv.setText("我才是真正的TextView");
     }
 }
